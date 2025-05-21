23f4484c478044872b54308c3b9f43da6996cd6a
AhmedSabry3232525 <abdoahmed95273@gmail.com>
2025-05-21 12:08:36 +0300

    server.py

--- server.py
+++ server.py
@@ -173,4 +173,5 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     os.makedirs(BASE_DIR, exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
